14f7a3f1a6843ab2abc166488f10e09f2747668f
G Mary Sravya <marysravya3@gmail.com>
2017-09-15 20:23:53 +0530

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+            Student s=new Student();
+		StudentGroup g=new StudentGroup();
+                g.getStudents();
+                g.getStudent(1);
+                g.addFirst(Srav);
+                g.addFirst(Sravs);
+                g.add(Srav,3);
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -26,48 +26,111 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student[] getStudents() {
 		// Add your implementation here
+                for(int i=0;i<students.length;i++)
+                    System.out.println(Student[i]);
 		return null;
 	}
 
 	@Override
 	public void setStudents(Student[] students) {
 		// Add your implementation here
+                if(students==null)
+                    throw IllegalArgumentException;
 	}
 
 	@Override
 	public Student getStudent(int index) {
 		// Add your implementation here
+                if((index<0)&&(index>=students.length))
+                    throw IllegalArgumentException;
 		return null;
 	}
 
 	@Override
 	public void setStudent(Student student, int index) {
 		// Add your implementation here
+                if((student == null)&&(index<0)&&(index>=students.length))
+                    throw IllegalArgumentException;
+                else
+                    students[index]=student;
 	}
 
 	@Override
 	public void addFirst(Student student) {
 		// Add your implementation here
+                if(student == null)
+                    throw IllegalArgumentException;
+                else
+                {
+                    for(int k=0;k<students.length;k++)
+                        students[k+1]=students[k];
+                    students[0]=student;
+                    students.length=students.length+1;
+                }
 	}
 
 	@Override
 	public void addLast(Student student) {
 		// Add your implementation here
+                if(student == null)
+                    throw IllegalArgumentException;
+                else
+                {
+                    students.length=students.length+1;
+                    students[students.length-1]=student;
+                }
 	}
 
 	@Override
 	public void add(Student student, int index) {
 		// Add your implementation here
+                if((student == null)&&(index<0)&&(index>=students.length))
+                    throw IllegalArgumentException;
+                else
+                {
+                    Student[index]=student;
+                    students.length=students.length + 1;
+                }
 	}
 
 	@Override
 	public void remove(int index) {
 		// Add your implementation here
+                if((student == null)&&(index<0)&&(index>=students.length))
+                    throw IllegalArgumentException;
+                else
+                {
+                for(int j=index; j<(students.length-1); j++)
+               {
+                   Student[j] = Student[j+1];
+               }
+                }
 	}
 
 	@Override
 	public void remove(Student student) {
 		// Add your implementation here
+                if(student == null)
+                    throw IllegalArgumentException;
+                else
+                {
+                for(int l=0; l<students.length; l++)
+       {
+           if(Student[l] == student)
+           {
+               for(int j=l; j<(students.length-1); j++)
+               {
+                   Student[j] = Student[j+1];
+               }
+               count++;
+               break;
+           }
+       }
+       if(count==0)
+       {
+           throw IllegalArgumentException("student not exist");
+       }
+                }    
 	}
 
 	@Override
@@ -109,31 +172,84 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public Student[] getNearBirthDate(Date date, int days) {
+            
 		// Add your implementation here
+                if (date == null)
+                    throw IllegalArgumentException;
+               
 		return null;
 	}
 
 	@Override
 	public int getCurrentAgeByDate(int indexOfStudent) {
 		// Add your implementation here
+                int ages=0;
+                if (indexOfStudent == 0)
+                    throw IllegalArgumentException;
+                else
+                {
+           for(int j=Student[indexOfStudent].birthDate;j<=2017;j++)
+           {
+               ages++;
+           }
+           System.out.println(ages);
 		return 0;
+                }
 	}
 
 	@Override
 	public Student[] getStudentsByAge(int age) {
 		// Add your implementation here
+                int ages=0;
+                         for(int i=0; i<students.length; i++)
+       {
+           for(int j=Student[i].birthDate;j<=2017;j++)
+           {
+               ages++;
+           if(ages == age)
+           {
+               return Student[i];
+           }  
+       }
+       }
 		return null;
 	}
 
 	@Override
 	public Student[] getStudentsWithMaxAvgMark() {
 		// Add your implementation here
+                 for(int i=0; i<students.length; i++)
+       {
+           if(Student[i].avgMark > Student[i+1].avgMark)
+           {
+               int max=Student[i].avgMark;
+           }
+       }
+                  for(i=0; i<students.length; i++)
+       {
+           if(Student[i].avgMark == max)
+           {
+               return Student[i];
+           }  
+       }
 		return null;
 	}
 
 	@Override
 	public Student getNextStudent(Student student) {
 		// Add your implementation here
+                if(student == null)
+                    throw IllegalArgumentException;
+                else
+                {
+                for(int i=0; i<size; i++)
+       {
+           if(Student[i] == student)
+           {
+               System.out.println(Student[i+1]);
+               break;
+           }
 		return null;
 	}
+                }
 }
